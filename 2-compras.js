353385daee82e55b7ff0eb1b76bb05336fa4ab14
felpiz-1 <rochadeandradef@gmail.com>
2026-02-23 19:35:33 -0300

    Exercicios

---
+++ 2-compras.js
@@ -0,0 +1,23 @@
+// --- Sistema de compras ---
+// O objetivo deste código é simular um sistema de compras simples, onde o usuário pode aplicar um desconto em um produto e atualizar o estoque.
+//  Identifique os erros presentes no código e corrija-os para que o sistema funcione corretamente.
+
+const produto = {
+  nome: "Teclado Gamer",
+  preco: 150.00,
+  estoque: 10,
+}
+
+// Desconto em porcentagem
+function aplicarDesconto(y) {
+  return produto.preco * 0.8;
+}
+
+
+const precoFinal = aplicarDesconto(produto.preco);
+
+produto.estoque = produto.estoque - 1 ;
+
+console.log("Produto: " + produto.nome);
+console.log("Preço com desconto: " + precoFinal);
+console.log("Estoque atual: " + produto.estoque);
---
+++ 4-moeda.js
@@ -0,0 +1,27 @@
+// --- Conversor de Moedas ---
+//  O objetivo deste código é simular um conversor de moedas simples, onde o usuário pode converter um valor em dólares para reais usando uma taxa de câmbio fixa.
+//  Identifique os erros presentes no código e corrija-os para que o conversor funcione corretamente.
+
+const taxaDolar = 5.50;
+
+function converterParaReal(valorDolar) {
+  let resultado = valorDolar * taxaDolar;
+  return resultado;
+}
+
+console.log("O valor convertido é de : R$ " + resultado);
+
+const transacao = {
+  usuario: "Carlos",
+  valor: "100,00",
+  concluida: true,
+};
+
+const valorFinal = converterParaReal;
+
+if (transacao.concluida == "sim") {
+  console.log("O valor convertido para " + transacao.usuario + " é: " + valorFinal);
+}
+if (transacao.concluida == "não") {
+  console.log("Erro no processamento.");
+}
